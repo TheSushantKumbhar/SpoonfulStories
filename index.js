c195b7c7c56d247c395be53ee70031570d1a4188
aditya-sutar-45 <aditya1.sutar@outlook.com>
2024-10-17 13:25:11 +0530

    login express setup

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
 app.listen(port, () => {
-  console.log("Listening on port 3000");
+  console.log(`Listening on port ${port}`);
 });
