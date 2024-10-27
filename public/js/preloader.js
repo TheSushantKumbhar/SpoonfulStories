7a97f1885cf8d8d86d0d5004d547eaf50bf6177e
aditya-sutar-45 <aditya1.sutar@outlook.com>
2024-10-27 16:24:46 +0530

    i dont remember lol

--- public/js/preloader.js
+++ public/js/preloader.js
@@ -1,14 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const preloader = document.getElementById('preloader');
-    const mainContent = document.getElementById('Main');
-  
-    window.addEventListener('load', () => {
-      preloader.classList.add('fade-out');
-  
-      setTimeout(() => {
-        preloader.style.display = 'none';
-        mainContent.style.display = 'block'; 
-      }, 1000); 
-    });
+document.addEventListener("DOMContentLoaded", () => {
+  const preloader = document.getElementById("preloader");
+  const mainContent = document.getElementById("Main");
+
+  window.addEventListener("load", () => {
+    preloader.classList.add("fade-out");
+
+    setTimeout(() => {
+      preloader.style.display = "none";
+      mainContent.style.display = "block";
+    }, 1000);
   });
-  
+});
