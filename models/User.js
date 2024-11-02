773a5b4017a69d9b155a66d58db661a848a7d01a
aditya-sutar-45 <aditya1.sutar@outlook.com>
2024-11-02 21:00:11 +0530

    Created a User Schema

---
+++ models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const UserSchmea = new Schema({
+  email: {
+    type: String,
+    required: [true, "email cannot be blank"],
+    unique: true,
+  },
+});
+
+// this will add on username, password fields to our schema
+UserSchmea.plugin(passportLocalMongoose); // it will also make sure usernames are unique
+// also gonna give us extra methods to use
+
+module.exports = mongoose.model("User", UserSchmea);
